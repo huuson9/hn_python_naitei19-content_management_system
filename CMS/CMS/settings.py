bc12cefa7cb6a3fe8058bf07c307ba450d362d1a
huuson9 <106168787+huuson9@users.noreply.github.com>
2023-09-13 12:39:09 +0700

    init models

--- CMS/CMS/settings.py
+++ CMS/CMS/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'home.User'
