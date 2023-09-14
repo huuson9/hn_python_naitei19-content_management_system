b2ece59c2a197674ce18e173154d2eb4c28ba1c9
huuson9 <106168787+huuson9@users.noreply.github.com>
2023-09-14 23:44:55 +0700

    Task_67987_QuanLyBaiViet

--- CMS/home/models.py
+++ CMS/home/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=150)
     email = models.EmailField()
-    num_rate_avg = models.IntegerField()
+    num_rate_avg = models.IntegerField(default=0)
 
     
     def __str__(self):
