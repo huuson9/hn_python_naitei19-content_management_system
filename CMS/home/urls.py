fac6a090bb4a8ed48dd161b34c9dc2e3769793ab
huuson9 <106168787+huuson9@users.noreply.github.com>
2023-10-04 02:36:25 +0700

    Task_68457_Filter_by_category, task_69692_Unit_test_register_form

--- CMS/home/urls.py
+++ CMS/home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.articleList, name='index'),
+    path('category/<str:category_name>/', views.articlesByCategory, name='articlesByCategory'),
     path('register/', views.sign_up, name='register'),
     path('user/<int:pk>/', views.showUserDetail, name='user'),
     path('userUpdate/<int:pk>/', views.updateUser, name='userUpdate'),
@@ -15,4 +16,3 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('rate/<int:post_id>/<int:rating>/', views.rateArticle, name='rateArticle'),
 ]
-
